19874303b8f6518602ba05338506e1729d9ebc44
manuelGT500 <manucb54@gmail.com>
2024-04-24 01:04:33 +0200

    FEATURE A DEVELOP

--- develop/proyecto/src/App.java
+++ develop/proyecto/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("ESTE ES EL PROYECTO DE HOTFIX");
+        System.out.println("ESTE ES EL PROYECTO DE FEATURE");
     }
 }
